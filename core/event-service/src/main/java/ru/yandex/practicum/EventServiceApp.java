38b6bbc03f02f5735aa5dc44aceeb12fb4f7f55b
Дмитрий Кан <dmitry.kan@yandex.ru>
2026-02-07 21:24:40 +0300

    fix: add basePackages to @EnableFeignClients annotation

--- core/event-service/src/main/java/ru/yandex/practicum/EventServiceApp.java
+++ core/event-service/src/main/java/ru/yandex/practicum/EventServiceApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "ru.yandex.practicum.client")
 public class EventServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApp.class, args);
