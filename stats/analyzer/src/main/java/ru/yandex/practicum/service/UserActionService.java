3e342bc25d9248aa23efa58f8bb37957323d92f1
Дмитрий Кан <dmitry.kan@yandex.ru>
2026-02-12 18:37:33 +0300

    feat: implement recommendation system (collector, aggregator, analyzer)

---
+++ stats/analyzer/src/main/java/ru/yandex/practicum/service/UserActionService.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.service;
+
+import ru.yandex.practicum.model.UserAction;
+
+public interface UserActionService {
+    void save(UserAction userAction);
+}
---
+++ stats/collector/src/main/java/ru/yandex/practicum/processor/UserActionProcessorImpl.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.processor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.practicum.ewm.stats.avro.UserActionAvro;
+import ru.practicum.ewm.stats.proto.UserActionProto;
+import ru.yandex.practicum.mapper.UserActionMapper;
+import ru.yandex.practicum.producer.UserActionProducer;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserActionProcessorImpl implements UserActionProcessor {
+
+    private final UserActionProducer producer;
+    private final UserActionMapper mapper;
+
+    @Override
+    public void process(UserActionProto userActionProto) {
+        UserActionAvro avro = mapper.mapToAvro(userActionProto);
+        producer.sendUserAction(avro);
+        log.debug("Действие пользователя обработано: userId={}", userActionProto.getUserId());
+    }
+}
